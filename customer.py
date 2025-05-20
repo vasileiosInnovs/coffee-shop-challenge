a803232f932f0ade1acc6bf9708541d4883d6eba
Hagee <hagee.ktr@outlook.com>
2025-05-20 13:07:44 +0300

    Generate code that stores the name of the customer, a getter and a setter that enforces rules

---
+++ customer.py
@@ -0,0 +1,39 @@
+from order import Order
+
+class Customer:
+    
+    all = []
+
+    def __init__(self, name):
+        self.name = name
+        Customer.all.append(self)
+
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, value):
+        if not isinstance(value, str):
+            raise ValueError ("The name has to be a string!")
+        if len(value) not in range(1, 16):
+            raise ValueError ("Name has to be 15 letters long!")
+        self._name = value
+
+    def create_order(self, coffee, price):
+        Order(self, coffee, price)
+
+    @classmethod
+    def most_aficionado(cls, coffee):
+        from collections import defaultdict
+
+        spending = defaultdict(float)
+
+        for order in Order.all:
+            if order.coffee == coffee:
+                spending[order.customer] += order.price
+
+        if not spending:
+            return None
+
+        return max(spending, key=spending.get)
