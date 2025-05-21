445e834d7e32967e0736abc386abb0d4b00f548b
Hagee <hagee.ktr@outlook.com>
2025-05-21 08:38:29 +0300

    Code tests that returns the Customer instance and returns Coffee instance

---
+++ tests/customer_test.py
@@ -0,0 +1,20 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from customer import Customer
+from coffee import Coffee
+from order import Order
+
+alice = Customer("Alice")
+bob = Customer("Bob")
+
+latte = Coffee("Latte")
+espresso = Coffee("Espresso")
+
+o1 = Order(alice, latte, 4.5)
+o2 = Order(alice, espresso, 3.0)
+o3 = Order(bob, latte, 5.0)
+
+print("Order.customer:", o1.customer.name)
+print("Order.coffee:", o1.coffee.name)
